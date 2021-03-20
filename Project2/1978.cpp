06b2455cf0f5bbc92dc75073b0e9598b551f4317
재형 전 <vnfmfms00@gmail.com>
2021-03-20 18:33:17 +0900

    소수 찾기 문제

--- Project2/1978.cpp
+++ Project2/1978.cpp
@@ -1,2 +1,34 @@
 #include <iostream>
 
+using namespace std;
+
+bool prime(int n)
+{
+	if (n < 2)
+		return false;
+	for (int i = 2; i * i <= n; i++)
+	{
+		if (n % i == 0)
+			return false;
+	}
+	return true;
+}
+
+int main()
+{
+	int t;
+	scanf("%d", &t);
+
+	int num;
+	int count = 0;
+
+	for (int i = 0; i < t; i++)
+	{
+		scanf("%d", &num);
+		if (prime(num))
+			count++;
+	}
+
+	printf("%d\n", count);
+	return 0;
+}
